85d335259141a2f20d90e4ae855aac3093622942
Payton Turnage <paytonturnage@gmail.com>
2014-07-05 23:21:27 -0700

    Use a standard KDF for deriving keys.
    This commit replaces the use of sha256 to derive keys with a standard kdf,
    PBKDF2 from PKCS#5.
    
    The defaults are:
    
    Hash algorithm for psuedo random numbers: HMAC-SHA1
    Iteration Depth: 2000
    Key length: 16 bytes for AES
    Salt: Empty

--- file_encryptor/key_generators.py
+++ file_encryptor/key_generators.py
@@ -1,19 +1,23 @@
+from Crypto.Protocol import KDF
 import hashlib
-import hmac
+import settings
 
-from settings import CHUNK_SIZE, DEFAULT_HMAC_PASSPHRASE
 
 def sha256_file(path):
     """Calculate sha256 hex digest of a file."""
     h = hashlib.sha256()
 
     with open(path) as f:
-        for chunk in iter(lambda: f.read(CHUNK_SIZE), b''):
+        for chunk in iter(lambda: f.read(settings.CHUNK_SIZE), b''):
             h.update(chunk)
 
     return h.hexdigest()
 
-def key_from_file(filename, passphrase):
+def key_from_file(filename,
+                  passphrase=None,
+                  salt=None,
+                  key_len=None,
+                  iteration_depth=None):
     """Calculate convergent encryption key.
 
     This takes a filename and an optional passphrase.
@@ -22,15 +26,27 @@ def key_from_file(filename, passphrase):
     vulnerable to confirmation attacks and
     learn-partial-information attacks.
 
+    Multiple arguments are also available for configuring
+    the employed KDF; including salt, key_len, and
+    iteration_depth. Currently employed is PBKDF2 from
+    PKCS #5, v2
+
     """
     hexdigest = sha256_file(filename)
 
     if passphrase is None:
-        passphrase = DEFAULT_HMAC_PASSPHRASE
+        passphrase = settings.DEFAULT_HMAC_PASSPHRASE
 
-    return keyed_hash(hexdigest, passphrase)
+    if salt is None:
+        salt = settings.DEFAULT_SALT
 
-def keyed_hash(hexdigest, passphrase):
-    """Calculate a HMAC/keyed hash."""
+    if key_len is None:
+        key_len = settings.DEFAULT_KEY_LEN
 
-    return hmac.new(passphrase, hexdigest, hashlib.sha256).digest()
+    if iteration_depth is None:
+        iteration_depth = settings.DEFAULT_ITERATION_DEPTH
+
+    return KDF.PBKDF2(passphrase + hexdigest,
+                      salt,
+                      key_len,
+                      iteration_depth)
